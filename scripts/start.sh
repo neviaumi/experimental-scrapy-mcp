a21b88853e7dad0a18b2836cf93590b286d152f2
David Ng <david.ng.dev@gmail.com>
2025-07-04 19:24:05 +0100

    Add support for dev and prod modes in `start.sh` with argument validation

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -ex
+MODE=${1:---dev}
 
-uv run fastapi dev --port 8080 src/app/main.py
+if [ "$MODE" == "--dev" ]; then
+  uv run fastapi dev --port 8080 src/app/main.py
+elif [ "$MODE" == "--prod" ]; then
+  uv run fastapi run --port 8080 src/app/main.py
+else
+    echo "Invalid argument: $MODE"
+    exit 1
+fi
+
