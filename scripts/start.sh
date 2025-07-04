e0c04ab3e5858e73c62571419458005de7d54b9b
David Ng <david.ng.dev@gmail.com>
2025-07-04 19:25:47 +0100

    Update prod mode in `start.sh` to use port 8081 instead of 8080.

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ MODE=${1:---dev}
 if [ "$MODE" == "--dev" ]; then
   uv run fastapi dev --port 8080 src/app/main.py
 elif [ "$MODE" == "--prod" ]; then
-  uv run fastapi run --port 8080 src/app/main.py
+  uv run fastapi run --port 8081 src/app/main.py
 else
     echo "Invalid argument: $MODE"
     exit 1
